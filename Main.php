2d8bf126a818793f1d83704c529275fba0226294
Nguyễn Quốc Thiện <quocthienagu@outlook.com>
2023-04-24 15:42:48 +0700

    abc

--- Main.php
+++ Main.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./css/main_slider.css">   
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+
+    <script src="https://kit.fontawesome.com/0f57b9b4e5.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="header">
@@ -17,11 +24,61 @@
         </div>
     </div>
     <div id="container">
+        <div class="slider">
+            <div class = "slider-prev"> 
+                <span id = "btnLeft">
+                    <i class="fa-solid fa-chevron-left"></i></div>
+                </span>
+                        
+        <!-- <i class="fa fa-angle-left slider-prev"></i> -->
+        <ul class="slider-dots">
+            <li class="slider-dot-item active" data-index="0"></li>
+            <li class="slider-dot-item" data-index="1"></li>
+            <li class="slider-dot-item" data-index="2"></li>
+            <li class="slider-dot-item" data-index="3"></li>
+            <li class="slider-dot-item" data-index="4"></li>
+        </ul>
+        <div class="slider-wrapper">
+            <div class="slider-main">
+            <div class="slider-item">
+                <img
+                src="https://images.unsplash.com/photo-1475809913362-28a064062ccd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                alt=""
+                />
+            </div>
+            <div class="slider-item">
+                <img
+                src="https://images.unsplash.com/photo-1463501810073-6e31c827a9bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                alt=""
+                />
+            </div>
+            <div class="slider-item">
+                <img
+                src="https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                alt=""
+                />
+            </div>
+            <div class="slider-item">
+                <img
+                src="https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80"
+                alt=""
+                />
+            </div>
+            <div class="slider-item">
+                <img
+                src="https://images.unsplash.com/photo-1470093851219-69951fcbb533?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                alt=""
+                />
+            </div>
+            </div>
+        </div>
+        <i class="fa fa-angle-right slider-next"></i>
+        </div>
+        <script src="./Js/starter.js"></script>
+        </div>
+        <div id="footer">
 
-    </div>
-    <div id="footer">
-
-    </div>
+        </div>
 
     
 
