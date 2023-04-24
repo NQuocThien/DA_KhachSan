0672bca033c8b457133408b6bae6a66fc3f0e248
Nguyễn Quốc Thiện <quocthienagu@outlook.com>
2023-04-24 13:11:05 +0700

    sường

--- Main.php
+++ Main.php
@@ -5,9 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    
+    <div id="header">
+        <div class="logo">
+                
+        </div>
+        <div class="navigation">
+
+        </div>
+    </div>
+    <div id="container">
+
+    </div>
+    <div id="footer">
+
+    </div>
+
     
 
 </body>
