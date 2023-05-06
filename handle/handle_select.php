278e39e489a50281f16eae429dd5f4aa3b5fff21
Nguyễn Quốc Thiện <quocthienagu@outlook.com>
2023-05-06 14:22:46 +0700

    abc

--- handle/handle_select.php
+++ handle/handle_select.php
@@ -3,8 +3,9 @@
     $key = $_POST['id'];
     $sql = "SELECT * FROM khachsan where MaChuoi = '$key'";
     $danhsach = $connect->query($sql);
-    while($row = $danhsach->fetch_array(MYSQLI_ASSOC))
+    while($row =  $danhsach->fetch_array(MYSQLI_ASSOC))
     {
+       // data 
        echo " <option value=".$row["MaKhachSan"]."> ".$row["TenKhachSan"] ." </option>" ;
     }
 ?>
