9fe874592de4c691aa605b2a31b90ddb5b58ca18
Nguyễn Quốc Thiện <quocthienagu@outlook.com>
2023-05-08 12:55:00 +0700

    tam

--- Main/insertphong.php
+++ Main/insertphong.php
@@ -26,23 +26,24 @@
             <p  ><i id="back"class="fa-solid fa-xmark"></i></p>
         </div>
         <div class="form_isphong_body">
-            <form action="" method="post">
-                <div class="choose">
-                        <label for="select_list-hotel">Chọn khách sạn:</label>
-                        <select name="select_list-hotel" id="select_list-hotel">
-                            <option value="0">----Chọn----</option>
-                            <?php 
-                                while($row1 = $sql_selectks->fetch(PDO::FETCH_ASSOC))
-                                {
-                                    echo "<option value =".$row1["MaKhachSan"].">".$row1["TenKhachSan"]."</option>"; ;
-                                }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="second_isphong">
-                    <div class="choose">
-                        <label for="select_list-hotel">Chọn Loại phòng:</label>
-                        <select name="select_list-hotel" id="select_list-hotel">
+            <form action="" method="post" id="form_themphong">
+                <div class="choose form_group">
+                    <label for="select_list-hotel">Chọn khách sạn:</label>
+                    <select name="select_list-hotel" id="select_list-hotel" class="form-control">
+                        <option value="0">----Chọn----</option>
+                        <?php 
+                            while($row1 = $sql_selectks->fetch(PDO::FETCH_ASSOC))
+                            {
+                                echo "<option value =".$row1["MaKhachSan"].">".$row1["TenKhachSan"]."</option>"; ;
+                            }
+                        ?>
+                    </select>
+                    <span class="form-message"></span>
+                </div>
+                <div class="second_isphong ">
+                    <div class="choose form_group">
+                        <label for="select_room-type">Chọn Loại phòng:</label>
+                        <select name="select_room-type" id="select_room-type" class="form-control">
                             <option value="0">----Chọn----</option>
                             <?php 
                                 while($row2 = $sql_selectloaiphong->fetch(PDO::FETCH_ASSOC))
@@ -51,31 +52,51 @@
                                 }
                             ?>
                         </select>
-                    </div>
-                    <div class="thongtinphong">
-                        <div class="ttpten">
-                            <span>Tên phòng:</span>
-                            <input type="text">
-                        </div>
-                        <div class="ttptinhtrang">
-                            <span>Tình trạng:</span>
-                            <input type="text">
-                        </div>
-                            
+                        <span class="form-message"></span>
 
                     </div>
+                    <div class="thongtinphong ">
+                        <div class="ttpten form_group">
+                            <label for = "room-name" >Tên phòng:</label>
+                            <input name = "room-name" type="text" id="room-name" class="form-control">
+                            <span class="form-message"></span>
+                        </div>
+                        <div class="ttptinhtrang form_group">
+                            <span class="">Tình trạng:</span>
+                            <input type="radio" class="form-control" id="trong" name="TinhTrang" value="Trống">
+                            <label for="trong">Trống</label><br>
+                            <input type="radio" class="form-control" id="DangO" name="TinhTrang" value="Đang ở">
+                            <label for="DangO">Đang ở</label><br>
+                            <input type="radio" class="form-control" id="datcoc" name="TinhTrang" value="Đặt cọc">
+                            <label for="datcoc">Đặt cọc</label><br>
+                            <span class="form-message"></span>
+                        </div>
+                    </div>
                 </div>
-                </div>
+                <button type="submit"> Thêm Phòng </button>
             </form>
         </div>
-                
-        </div>
     </div>
-    <script>
-        var test= document.getElementById('back');
-                        test.addEventListener('click',()=>{
-                            window.history.back();
-                        });
-                    
-    </script>
+</div>
+<script src="../Js/validator.js"></script>
+<script>
+    Validator({
+        form: '#form_themphong',
+        orrorSelector: '.form-message',
+        formGroupSelector: '.form_group' ,
+        rules: [
+        Validator.isRequired('#select_list-hotel'),
+        Validator.isRequired('#select_room-type'),
+        Validator.isRequired('#room-name'),
+        Validator.isRequired('input[name="TinhTrang"]'),
+        ]
+    }) 
+</script>
+<script>
+    var test= document.getElementById('back');
+                    test.addEventListener('click',()=>{
+                        window.history.back();
+                    });
+                
+</script>
 </div>
