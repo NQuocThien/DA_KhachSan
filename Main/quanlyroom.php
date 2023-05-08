17c1528225dc3671f5d39a1b8cf7100af2e22cfe
Nguyễn Quốc Thiện <quocthienagu@outlook.com>
2023-05-08 11:12:49 +0700

    phong

--- Main/quanlyroom.php
+++ Main/quanlyroom.php
@@ -1,8 +1,7 @@
 <?php
      include "../Chung/cauhinh.php";
     //  session_start();
-     $stmt = $pdo->prepare("SELECT DISTINCT ks.`MaKhachSan`,ks.`TenKhachSan` FROM KhachSan ks, Phong p WHERE ks.`MaKhachSan` = p.`MaKhachSan`");	
-     $stmt->execute();
+     
      
 
 ?>
@@ -10,21 +9,30 @@
     <div class="header_room">
         <div class="hr_firt">
             <p>Quản lý phòng</p>
+            <div class="select_chuoi">
+                <select name="chuoi" id="select_chuoiKS">
+                    <option value="0"> ---- chon ---</option>
+                    <?php 
+                        $sql = "SELECT * FROM chuoikhachsan ";
+                        $danhsach = $connect -> query($sql);
+                        while ($row = $danhsach->fetch_array(MYSQLI_ASSOC))
+                        {
+                            echo "<option value= ".$row["MaChuoi"].">".$row["TenChuoi"]."</option>";
+                        }
+                    ?>
+                </select>
+            </div>
             <div class="menuroom">
                 
             <div class="menu_iconroom ">
                 <i id="my_menu" class="fa-solid fa-bars"></i>
                  <div  id = "nav_menuroo"class="nav_menuroom ">
                     <a data-target="insertphong.php" class="myLink" >Loại phòng</a>
-                    <!-- <a data-target="" class="myLink" >Loại phòng</a>
-                    <a data-target="" class="myLink" >Loại phòng</a> -->
                 </div>
                 
             </div>
                 
             </div>
-          
-            
         </div>
         <div class="hr_second">
             <div class="icon_trangthairoom"><p></p><span>:Trống</span></div>
@@ -37,29 +45,13 @@
     </div>
     <div class="form_room"></div>
     <div class="body_room">
-            <div class="br_firt" id = "br_firt">
-                <?php
-                    include "../Chung/cauhinh.php";
-                    while ($dong = $stmt->fetch(PDO::FETCH_ASSOC)){
-                        $stmt1 = $pdo->prepare("SELECT COUNT(ks.`MaKhachSan`)as'tongphong' from KhachSan ks, Phong p WHERE ks.`MaKhachSan` = p.`MaKhachSan` AND ks.`TenKhachSan`=:value");
-                        $stmt1->bindParam(':value', $dong['TenKhachSan']);
-                        $stmt1->execute();
-                        $dong1 = $stmt1->fetch(PDO::FETCH_ASSOC);
-                       
-				        $_SESSION['MaKS'.$dong['MaKhachSan'].''] = $dong['MaKhachSan'];
-                        echo  "<div class=\"room_mylist\">
-                        <i class=\"fa-solid fa-hotel\"></i>
-                     <a id=\"aroom_mylist\"class=\"myLink\" data-target=\"hienthiphong.php?do=&id=".$dong['MaKhachSan']."\">".$dong['TenKhachSan']."</a>
-                     <span>" .$dong1['tongphong']."</span>
-                    </div>";
-                    }
-                ?>
            
-            </div>
-            <div class="br_second" id="br_second">
-                
-            </div>
             
     </div>
+ 
+    <script type="module" >
+        import {myAjax} from "../handle/ajax.js";
+        myAjax('.main_quanlroom .header_room #select_chuoiKS',".main_quanlroom .body_room", "../handle/handle_change_body-room.php" );
+    </script>
     
 </div>
--- handle/ajax.js
+++ handle/ajax.js
@@ -6,6 +6,7 @@ export const myAjax = (parentElement, currentElement, pathFile)=>
             $.post(pathFile,{id : id} , function(date)
             {
                 $(currentElement).html(date);
+                
                  });
             })
         })
